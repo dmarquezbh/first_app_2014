cbfeca50e5c544750a5400106448ce259c65484e
Daniel Marques <dmarques.bh@gmail.com>
2014-04-03 19:57:13 -0300

    Adiciona gender para tabela users

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140403220838) do
-
-  create_table "daniels", force: true do |t|
-    t.string   "teste"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20140403225448) do
 
   create_table "users", force: true do |t|
     t.string   "name",       null: false
-    t.string   "email"
+    t.string   "email",      null: false
     t.integer  "age"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "gender"
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,18 @@ describe User do
     user.save.should be_true
   end
 
+  it 'fails to create an user with blank name' do
+    user = User.new email: "jlucasps@gmail.com", age: 25
+    user.save.should be_false
+  end
+
+  it 'fails to create an user with blank email' do
+    user = User.new name: "João Lucas", age: 25
+    user.save.should be_false
+  end
+
+  it 'creates a user with gender value FEMALE' do
+    user = User.new name: "Candice", email: "candice@gmail.com", gender: User::FEMALE
+    user.save.should be_true
+  end
 end
