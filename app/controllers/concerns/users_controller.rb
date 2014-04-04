fb40c2a36648b28a6903263d85b917b115fa4021
Daniel Marques <dmarques.bh@gmail.com>
2014-04-04 19:54:50 -0300

    Adiciona localização

--- app/controllers/concerns/users_controller.rb
+++ app/controllers/concerns/users_controller.rb
@@ -20,6 +20,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :age)
+    params.require(:user).permit(:name, :email, :age, :gender)
   end
 end
