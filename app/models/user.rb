543b3d84765e75734dc79857717b1153621d8b32
Daniel Marques <dmarques.bh@gmail.com>
2014-04-08 22:29:51 -0300

    Autenticação por devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
   FEMALE = 1
   MALE = 2
@@ -6,7 +10,7 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, allow_blank: false
   validates :email, presence: true, allow_blank: false
-  validates :age, presence: true, allow_blank: false
+  #validates :age, presence: true, allow_blank: false
   validates :gender, presence: true, if: :adulthood
   
   validates_uniqueness_of :email
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,4 +29,15 @@ FirstApp::Application.configure do
 
   #config.log_level = :debug
   #config.active_record.logger = Logger.new(STDOUT)
+
+  config.action_mailer.default_url_options = {
+    :host => 'localhost:3000'
+  }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :address => 'localhost',
+    :port => 1025
+  }
 end
